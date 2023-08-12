1e0a57c6347bb2de1674c835162b9c8662e8e2ea
DanilaDool <Levhenko-2626@mail.ru>
2023-08-13 00:32:44 +0300

    add validate

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,15 @@
-class Question < ApplicationRecord
+class Question < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates :text, :user, presence: true
+
+  ['validation', 'save', 'create', 'update', 'destroy'].each do |action|
+    ['before', 'after'].each do |time|
+      define_method("#{time}_#{action}") do
+        puts "****>#{time} #{action}"
+    end
+  end
+end
+
 end
